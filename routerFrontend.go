e038855273abccdfea7dcf5e00985bd4e251763a
ShadiestGoat <48590492+ShadiestGoat@users.noreply.github.com>
2022-11-08 13:42:33 +0000

    Add goal components

--- routerFrontend.go
+++ routerFrontend.go
@@ -17,9 +17,11 @@ import (
 var (
 	MAIN_CSS       []byte
 	PAGE_FUND      []byte
-	PAGE_GOAL_FUND []byte
+	GOAL_COMPONENT []byte
 	FUNDS          []byte
 	DEFAULT_PFP    []byte
+
+	GOAL_COMPONENT_WIDTH = 118.0
 )
 
 func InitFrontend() {
@@ -34,9 +36,11 @@ func InitFrontend() {
 		"PP_CLIENT_ID": []byte(PAYPAL_CLIENT_ID),
 	})
 
-	b, err = os.ReadFile("pages/goalFund.html")
+	b, err = os.ReadFile("pages/compGoal.html")
 	PanicIfErr(err)
-	PAGE_GOAL_FUND = Template(b, map[string][]byte{})
+	GOAL_COMPONENT = Template(b, map[string][]byte{
+		"GOAL_WIDTH": []byte(fmt.Sprint(GOAL_COMPONENT_WIDTH)),
+	})
 
 	DEFAULT_PFP, err = os.ReadFile("pages/defaultPFP.png")
 	PanicIfErr(err)
@@ -98,6 +102,39 @@ func FrontendFund(w http.ResponseWriter, r *http.Request, fundID string) {
 		dID, discordName, discordPFP = FetchDiscordUser(dID, "Bot "+DISCORD_TOKEN)
 	}
 
+	goalComp := []byte{}
+
+	if fund.Goal != 0 {
+		total := 0.0
+		err := DBQueryRow(`SELECT SUM(amount_received) AS total FROM donations`).Scan(&total)
+		if err != nil {
+			logger.Logf(LL_ERROR, "Bad goal fetch: %v", err)
+			http.Redirect(w, r, "/error?err=Unknown%20Error", http.StatusTemporaryRedirect)
+			return
+		}
+
+		perc := total / fund.Goal
+		width := perc * GOAL_COMPONENT_WIDTH
+
+		if perc > 1 {
+			width = GOAL_COMPONENT_WIDTH
+		}
+
+		textXOffset := perc * 100
+
+		if perc > 1 || perc < 0.25 {
+			textXOffset = 50
+		}
+
+		text := fmt.Sprintf(`<text x="%.2f%%" y="50%%" fill="#fff" font-family="sans-serif" font-size="9" dominant-baseline="middle" text-anchor="middle">%.0f%%</text>`, textXOffset, perc*100)
+
+		goalComp = Template(GOAL_COMPONENT, map[string][]byte{
+			"WIDTH":    []byte(fmt.Sprintf(`%.2f`, width)),
+			"TEXT_SVG": []byte(text),
+			"GOAL_MAX": []byte(fmt.Sprint(fund.Goal)),
+		})
+	}
+
 	Respond(w, 200, Template(PAGE_FUND, map[string][]byte{
 		"FUND_NAME": []byte(fund.Name),
 		"FUND_DESC": []byte(fund.Description),
@@ -106,6 +143,7 @@ func FrontendFund(w http.ResponseWriter, r *http.Request, fundID string) {
 		"D_NAME":    []byte(discordName),
 		"D_PFP":     []byte(discordPFP),
 		"D_ID":      []byte(dID),
+		"GOAL_COMP": goalComp,
 	}))
 }
 
